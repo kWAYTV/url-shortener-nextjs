2a33e0ee673e1d96b12756d8d3f3d2bd033a0cc2
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-06-02 21:24:26 +0200

    fix(schema): enforce minimum length for custom code in URL schemas
    - Added validation to ensure custom code is at least 3 characters long in both `urlSchema` and `updateUrlSchema`.

--- src/schemas/url.schema.ts
+++ src/schemas/url.schema.ts
@@ -6,6 +6,7 @@ export const urlSchema = z.object({
   url: z.string().min(1, 'URL is required'),
   customCode: z
     .string()
+    .min(3, 'Custom code must be at least 3 characters')
     .max(20, 'Custom code must be less than 255 characters')
     .regex(/^[a-zA-Z0-9_-]+$/, 'Custom code must be alphanumeric or hyphen')
     .optional()
@@ -27,6 +28,7 @@ export const updateUrlSchema = z.object({
   id: z.coerce.number(),
   customCode: z
     .string()
+    .min(3, 'Custom code must be at least 3 characters')
     .max(255, 'Custom code must be less than 255 characters')
     .regex(/^[a-zA-Z0-9_-]+$/, 'Custom code must be alphanumeric or hyphen')
 });
