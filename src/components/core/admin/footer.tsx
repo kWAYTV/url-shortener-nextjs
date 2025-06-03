a1181889fc8b65cb8da11734775693b785a47816
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-06-03 14:48:56 +0200

    feat(footer): update footer layout and content for improved navigation
    - Redesigned footer to include navigation links for 'short', 'Dashboard', and 'Admin'.
    - Added a visual element with a heart icon to enhance the footer's aesthetic.
    - Removed previous text content to streamline the footer's purpose.

--- src/components/core/admin/footer.tsx
+++ src/components/core/admin/footer.tsx
@@ -1,30 +1,37 @@
+import { Heart } from 'lucide-react';
 import Link from 'next/link';
 
 export function Footer() {
   return (
     <div className='bg-background/95 supports-[backdrop-filter]:bg-background/60 z-20 w-full shadow backdrop-blur'>
       <div className='mx-4 flex h-14 items-center md:mx-8'>
-        <p className='text-muted-foreground text-left text-xs leading-loose md:text-sm'>
-          Built on top of{' '}
-          <Link
-            href='https://ui.shadcn.com'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='font-medium underline underline-offset-4'
-          >
-            shadcn/ui
-          </Link>
-          . The source code is available on{' '}
-          <Link
-            href='https://github.com/salimi-my/shadcn-ui-sidebar'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='font-medium underline underline-offset-4'
-          >
-            GitHub
-          </Link>
-          .
-        </p>
+        <div className='text-muted-foreground flex flex-1 items-center justify-between text-sm'>
+          <div className='flex items-center gap-4'>
+            <Link
+              href='/'
+              className='text-foreground text-lg font-bold transition-transform duration-200 ease-in-out hover:scale-110'
+            >
+              short
+            </Link>
+            <span className='hidden md:inline'>•</span>
+            <Link
+              href='/dashboard'
+              className='hover:text-foreground transition-colors'
+            >
+              Dashboard
+            </Link>
+            <span>•</span>
+            <Link
+              href='/admin'
+              className='hover:text-foreground transition-colors'
+            >
+              Admin
+            </Link>
+          </div>
+          <div className='hidden items-center gap-1 md:flex'>
+            Made with <Heart className='size-4 text-red-500' /> using Next.js
+          </div>
+        </div>
       </div>
     </div>
   );
